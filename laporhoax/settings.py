3d003b17bf755bddb5ffd6c0682906d04cacd384
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-09-13 01:09:48 +0700

    update settings1

--- laporhoax/settings.py
+++ laporhoax/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-tn!@g=m!j2p-*=uy=&d2l&w*8khe%l&ggyc-+i7_@w0&r-o$n2'
-SECRET_KEY = os.environ.get('DjangoKey')
+SECRET_KEY = 'django-insecure-tn!@g=m!j2p-*=uy=&d2l&w*8khe%l&ggyc-+i7_@w0&r-o$n2'
+#SECRET_KEY = os.environ.get('DjangoKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
