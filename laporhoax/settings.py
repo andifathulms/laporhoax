f77f1dd097050c70c8544699c4376222a66cdd92
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-09-13 01:06:02 +0700

    update settings

--- laporhoax/settings.py
+++ laporhoax/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tn!@g=m!j2p-*=uy=&d2l&w*8khe%l&ggyc-+i7_@w0&r-o$n2'
-
+#SECRET_KEY = 'django-insecure-tn!@g=m!j2p-*=uy=&d2l&w*8khe%l&ggyc-+i7_@w0&r-o$n2'
+SECRET_KEY = os.environ.get('DjangoKey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['laporhoaxpnp.herokuapp.com']
 
 
 # Application definition
@@ -156,9 +156,13 @@ PHONENUMBER_DEFAULT_REGION = 'ID'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rubyghozal@gmail.com'
-EMAIL_HOST_PASSWORD = 'sapibetina'
+#EMAIL_HOST_USER = 'rubyghozal@gmail.com'
+#EMAIL_HOST_PASSWORD = 'sapibetina'
+EMAIL_HOST_USER = os.environ.get('EmailDjango')
+EMAIL_HOST_PASSWORD = os.environ.get('EmailPwdDjango')
 EMAIL_PORT = 587
+
+
 
 #allauth addition
 
